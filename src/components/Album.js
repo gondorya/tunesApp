a5b0f327ccf965b3c4d3b38f2c9e101dd470914e
gondorya <monia87f@gmail.com>
2019-01-02 17:25:30 +0100

    cleanup

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const Album = (props) => (
-  <div className={`col col-12 col-md-6 col-lg-4 albumList__item ${props.stateClass}`} 
-    onClick={() => {props.activateTile(props.index)}} 
-    onKeyPress={(e) => {props.keyPress(e, '13', props.activateTile(props.index))}}
-    tabIndex={props.tabIndexValue}
-    data-index={props.index + 1}>
-    <button className="button button--close" onClick={(e) => {e.stopPropagation(); props.activateTile(-1)}}>close</button>
-    <div className="albumList__image" style={props.styles}></div>
-    <div className="albumList__content">
-      <p><span className="albumList__label">Title:</span> {props.title}</p>
-      <p><span className="albumList__label">Artist:</span> {props.artist}</p>
-      <p><span className="albumList__label">Released:</span> {props.date}</p>
-      <p><span className="albumList__label">Category:</span> {props.category}</p>
-      <p><a href={props.link}>Go to album</a></p>
+    <div className={`col col-12 col-md-6 col-lg-4 albumList__item ${props.stateClass}`} 
+        onClick={() => {props.activateTile(props.index)}} 
+        onKeyPress={(e) => {props.keyPress(e, '13', props.activateTile(props.index))}}
+        tabIndex={props.tabIndexValue}
+        data-index={props.index + 1}>
+        <button className="button button--close" onClick={(e) => {e.stopPropagation(); props.activateTile(-1)}}>close</button>
+        <div className="albumList__image" style={props.styles}></div>
+        <div className="albumList__content">
+            <p><span className="albumList__label">Title:</span> {props.title}</p>
+            <p><span className="albumList__label">Artist:</span> {props.artist}</p>
+            <p><span className="albumList__label">Released:</span> {props.date}</p>
+            <p><span className="albumList__label">Category:</span> {props.category}</p>
+            <p><a href={props.link}>Go to album</a></p>
+        </div>
     </div>
-  </div>
 );
 
 export default Album;
--- src/components/GetAlbums.js
+++ src/components/GetAlbums.js
@@ -4,69 +4,69 @@ import AlbumsNavigation from './AlbumsNavigation';
 
 const API = 'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
 const CLASSES = {
-  isActive: 'is-active',
-  isVisible: 'is-visible',
+	isActive: 'is-active',
+  	isVisible: 'is-visible',
 }
 
 export default class GetAlbums extends React.Component {
-  constructor(props) {
-    super(props);
+  	constructor(props) {
+		super(props);
 
-    this.state = {
-      albums: [],
-      activeItem: -1,
-    };
-  }
+		this.state = {
+	  		albums: [],
+	  		activeItem: -1,
+		};
+  	}
 
-  componentDidMount() {
-    fetch(API)
-      .then(response => response.json())
-      .then(data => this.setState({ albums: data.feed.entry }))
-      .catch(error => console.error('Error:', error));
-  }
+  	componentDidMount() {
+		fetch(API)
+		  	.then(response => response.json())
+		  	.then(data => this.setState({ albums: data.feed.entry }))
+		  	.catch(error => console.error('Error:', error));
+	  	}
 
-  activateTile = (index) => {
-    this.setState({
-      activeItem: index,
-    })
-  }
+	  	activateTile = (index) => {
+			this.setState({
+		  		activeItem: index,
+			})
+	  	}
 
-  handleKeyPress = (e, keyCode, action) => {
-    if (e.keyCode === keyCode) {
-      action;
-    }
-  }
+	  	handleKeyPress = (e, keyCode, action) => {
+			if (e.keyCode === keyCode) {
+		  		action;
+			}
+	  	}
 
-  render() {
-    const { albums } = this.state;
-    return (
-      <div className="albumList row">
-        {albums.map((album, index) => {
-          const pic = album['im:image'][2].label.replace('170x170', '350x350');
-          return (
-            <Album
-              key={album.id.attributes['im:id']}
-              stateClass={this.state.activeItem === index ? CLASSES.isActive : ''}
-              tabIndexValue={this.state.activeItem === -1 ? '1' : false}
-              activateTile={this.activateTile}
-              index = {index}
-              title={album['im:name'].label}
-              artist={album['im:artist'].label}
-              date={album['im:releaseDate'].attributes.label}
-              category={album.category.attributes.label}
-              link={album.link.attributes.href}
-              styles={{backgroundImage: `url(${pic})`}} 
-              keyPress = {this.handleKeyPress}/>
-        )})}
+	  	render() {
+			const { albums } = this.state;
+			return (
+		  	<div className="albumList row">
+				{albums.map((album, index) => {
+			  		const pic = album['im:image'][2].label.replace('170x170', '350x350');
+			  		
+			  		return (
+						<Album
+							key={album.id.attributes['im:id']}
+							stateClass={this.state.activeItem === index ? CLASSES.isActive : ''}
+							tabIndexValue={this.state.activeItem === -1 ? '1' : false}
+							activateTile={this.activateTile}
+							index = {index}
+							title={album['im:name'].label}
+							artist={album['im:artist'].label}
+							date={album['im:releaseDate'].attributes.label}
+							category={album.category.attributes.label}
+							link={album.link.attributes.href}
+							styles={{backgroundImage: `url(${pic})`}} 
+							keyPress = {this.handleKeyPress}/>
+				)})}
 
-        <AlbumsNavigation 
-          stateClass={this.state.activeItem > -1 ? CLASSES.isVisible : ''}
-          prevStateClass={this.state.activeItem > 0 ? CLASSES.isActive : ''}
-          nextStateClass={this.state.activeItem < 99 ? CLASSES.isActive : ''}
-          currentIndex={this.state.activeItem}
-          showTile={(newIndex) => {this.activateTile(newIndex)}}
-        />
-      </div>
-    );
-  }
+				<AlbumsNavigation 
+					stateClass={this.state.activeItem > -1 ? CLASSES.isVisible : ''}
+					prevStateClass={this.state.activeItem > 0 ? CLASSES.isActive : ''}
+					nextStateClass={this.state.activeItem < 99 ? CLASSES.isActive : ''}
+					currentIndex={this.state.activeItem}
+					showTile={(newIndex) => {this.activateTile(newIndex)}}/>
+		  	</div>
+		);
+	}
 };
